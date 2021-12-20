c7cdd4226022db0866e06b4ba1926b8b11be3bcb
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-20 23:12:13 +0100

    para que se haga una estrella, como mínimo tiene que tener 5 esquinas

--- estrellasregulares.py
+++ estrellasregulares.py
@@ -2,9 +2,15 @@
 
 import turtle
 
-def estrella(puntas, lado = 150):
+def estrella(esquinas, lado = 150):
 
     def mcd(x,y):
         while y != 0:
             x, y = y, x % y
             return x
+
+    if esquinas <= 4:
+        print("Introduce un número de esquinas mayor.")
+        return
+
+
