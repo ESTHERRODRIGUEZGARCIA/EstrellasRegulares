a7881a270269344387f62540de7fbfc7401aa266
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-20 22:34:05 +0100

    guardar

--- estrellasregulares.py
+++ estrellasregulares.py
@@ -3,12 +3,5 @@
 from turtle import *
 
 def estrella(n):
-    def mcm(x,y):
-        while y != 0:
-            x, y = y, x % y
-            return x
-        
-    def angulo():
-        angulo = 0
-        if n % 4 == 0:
             
+    
