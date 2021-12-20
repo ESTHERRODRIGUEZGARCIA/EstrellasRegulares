15b780f04c0cb8e0e7445dae46befb966b28d819
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2021-12-20 22:37:43 +0100

    introduce el numero de puntas d ela estrella

--- estrellasregulares.py
+++ estrellasregulares.py
@@ -2,6 +2,24 @@
 
 from turtle import *
 
+n = int(input("Introduzca el número de puntas de la estrella: "))
+
 def estrella(n):
             
-    
+    def angulo():
+        angulo = 0
+        if n % 4 == 0:
+            angulo = 180 - (360/n) 
+        elif n % 3 == 0:
+            angulo = 120 + (360/n)
+        else:
+            pass
+        return angulo
+        
+turtle.speed(0)
+for _ in range(n):
+    turtle.right(angulo())
+    turtle.fordward(200)
+turtle.done()
+
+
